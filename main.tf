47ff872844e1848a33916baa1abc387322ed030d
hogehoge789 <hogehoge789@gmail.com>
2017-09-29 02:21:47 +0900

    Changes to be committed: 	modified:   .gitignore 	modified:   main.tf 	new file:   variables.tf

--- main.tf
+++ main.tf
@@ -1,23 +1,3 @@
-variable "access_key" {}
-variable "secret_key" {}
-variable "aws_region" {
-    default = "ap-northeast-1"
-}
-
-variable "key_name" {
-  description = "Desired name of AWS key pair"
-}
-
-variable "public_key_path" {
-  description = <<DESCRIPTION
-Path to the SSH public key to be used for authentication.
-Ensure this keypair is added to your local SSH agent so provisioners can
-connect.
-
-Example: ~/.ssh/terraform.pub
-DESCRIPTION
-}
-
 provider "aws" {
   access_key = "${var.access_key}"
   secret_key = "${var.secret_key}"
@@ -42,10 +22,18 @@ resource "aws_route" "tf-internet_access" {
 }
 
 # サブネットの作成
-resource "aws_subnet" "tf-subnet" {
+resource "aws_subnet" "tf-subnet-001" {
   vpc_id                  = "${aws_vpc.tf-vpc.id}"
   cidr_block              = "10.1.1.0/24"
   map_public_ip_on_launch = true
+  availability_zone = "ap-northeast-1a"
+}
+
+resource "aws_subnet" "tf-subnet-002" {
+  vpc_id                  = "${aws_vpc.tf-vpc.id}"
+  cidr_block              = "10.1.10.0/24"
+  map_public_ip_on_launch = true
+  availability_zone = "ap-northeast-1c"
 }
 
 # EC2用セキュリティグループの作成(SSH and HTTP)
@@ -62,12 +50,12 @@ resource "aws_security_group" "tf-sec" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  # HTTP access from the VPC
+  # HTTP access from anywhere
   ingress {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
-    cidr_blocks = ["10.0.0.0/16"]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   # outbound internet access
@@ -79,26 +67,83 @@ resource "aws_security_group" "tf-sec" {
   }
 }
 
-# 鍵作成
+# 鍵インポート
 resource "aws_key_pair" "auth" {
   key_name   = "${var.key_name}"
-  public_key = "${file(var.public_key_path)}"
+  public_key = "${var.public_key}"
 }
 
 # インスタンスの作成
-resource "aws_instance" "tf-aws" {
+resource "aws_instance" "tf-ec2-01" {
   ami           = "ami-4af5022c"
   instance_type = "t2.small"
-  subnet_id = "${aws_subnet.tf-subnet.id}"
+  subnet_id = "${aws_subnet.tf-subnet-001.id}"
   key_name      = "${aws_key_pair.auth.id}"
   vpc_security_group_ids = ["${aws_security_group.tf-sec.id}"]
+  availability_zone = "ap-northeast-1a"
+  root_block_device {
+    volume_size = "50"
+  }
+  tags {
+      "Name" = "terraform-001"
+  }
+}
 
+resource "null_resource" "post_process-01" {
+  triggers {
+    instance = "${aws_instance.tf-ec2-01.public_ip}"
+  }
   provisioner "remote-exec" {
+    connection {
+      type        = "ssh"
+      host        = "${aws_instance.tf-ec2-01.public_ip}"
+      user        = "ec2-user"
+      private_key = "${file("terraform.ppk")}"
+    }
     inline = [
       "sudo sed -i -e 's/forced-commands-only/without-password/' /etc/ssh/sshd_config",
-      "suro cp -f /home/ec2-user/.ssh/authorized_keys /root/.ssh/authorized_keys",
+      "sudo cp -f /home/ec2-user/.ssh/authorized_keys /root/.ssh/authorized_keys",
       "sudo service sshd reload",
       "sudo sed -i -e 's/disable_root: false/disable_root: true/' /etc/cloud/cloud.cfg"
     ]
   }
 }
+
+resource "aws_instance" "tf-ec2-02" {
+  ami           = "ami-4af5022c"
+  instance_type = "t2.small"
+  subnet_id = "${aws_subnet.tf-subnet-002.id}"
+  key_name      = "${aws_key_pair.auth.id}"
+  vpc_security_group_ids = ["${aws_security_group.tf-sec.id}"]
+  availability_zone = "ap-northeast-1c"
+  root_block_device {
+    volume_size = "50"
+  }
+  tags {
+      "Name" = "terraform-002"
+  }
+}
+
+resource "null_resource" "post_process-02" {
+  triggers {
+    instance = "${aws_instance.tf-ec2-02.public_ip}"
+  }
+  provisioner "remote-exec" {
+    connection {
+      type        = "ssh"
+      host        = "${aws_instance.tf-ec2-02.public_ip}"
+      user        = "ec2-user"
+      private_key = "${file("terraform.ppk")}"
+    }
+    inline = [
+      "sudo sed -i -e 's/forced-commands-only/without-password/' /etc/ssh/sshd_config",
+      "sudo cp -f /home/ec2-user/.ssh/authorized_keys /root/.ssh/authorized_keys",
+      "sudo service sshd reload",
+      "sudo sed -i -e 's/disable_root: false/disable_root: true/' /etc/cloud/cloud.cfg",
+      "sudo yum -y install nginx",
+      "sudo service nginx start",
+      "sudo chkconfig nginx on"
+    ]
+  }
+}
+
---
+++ variables.tf
@@ -0,0 +1,8 @@
+variable "access_key" {}
+variable "secret_key" {}
+variable "aws_region" {
+    default = "ap-northeast-1"
+}
+
+variable "key_name" {}
+variable "public_key" {}
